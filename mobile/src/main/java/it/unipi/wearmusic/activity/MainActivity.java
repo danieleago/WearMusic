1f3d8b4637ea5030058205002e6db078befe99db
danieleago <agostidaniele5@gmail.com>
2017-09-13 08:49:13 +0200

    restructuring mobile code

--- mobile/src/main/java/it/unipi/wearmusic/activity/MainActivity.java
+++ mobile/src/main/java/it/unipi/wearmusic/activity/MainActivity.java
@@ -2,7 +2,6 @@ package it.unipi.wearmusic.activity;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.ClipData;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -10,7 +9,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,31 +16,18 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.view.menu.ActionMenuItem;
-import android.support.v7.view.menu.ActionMenuItemView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.MediaController.MediaPlayerControl;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
-import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.ArrayList;
@@ -50,8 +35,6 @@ import java.util.Collections;
 import java.util.Comparator;
 
 import it.unipi.wearmusic.R;
-import it.unipi.wearmusic.util.ListenerService;
-import it.unipi.wearmusic.util.MusicController;
 import it.unipi.wearmusic.util.MusicService;
 import it.unipi.wearmusic.util.Song;
 import it.unipi.wearmusic.util.SongAdapter;
@@ -72,9 +55,6 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
     private MusicService musicSrv;
     private Intent playIntent;
     private boolean musicBound=false;
-    //private MusicController controller;
-    //private boolean paused=true;
-    private static final String COMMAND_KEY = "command";
     private GoogleApiClient mGoogleApiClient;
     private AudioManager managerAudio;
     //connect to the service
@@ -96,6 +76,8 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         }
     };
 
+    // ----------------------- Override Activity methods --------------------------------------------------------
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -107,6 +89,7 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
             startService(playIntent);
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -129,14 +112,249 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
             }
         });
 
-
         SongAdapter songAdt = new SongAdapter(this, songList);
-
         songView.setAdapter(songAdt);
 
-        //setController();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_shuffle:
+                musicSrv.setShuffle();
+                if (musicSrv.isShuffle()){
+                    item.setIcon(ContextCompat.getDrawable(this,R.drawable.img_btn_shuffle_pressed));
+                } else {
+                    item.setIcon(ContextCompat.getDrawable(this,R.drawable.img_btn_shuffle));
+                }
+                break;
+
+            case R.id.action_end:
+                stopService(playIntent);
+                musicSrv=null;
+                System.exit(0);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //paused=true;
+        //Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+        //mGoogleApiClient.disconnect();
 
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+
+        super.onStop();
+    }
+
+    // ---------------------------- Override MediaPlayerControl methods --------------------------------------
+
+    @Override
+    public void pause() {
+        musicSrv.pausePlayer();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        musicSrv.seek(pos);
+    }
+
+    @Override
+    public void start() {
+        musicSrv.go();
+    }
+
+    @Override
+    public int getDuration() {
+        if(musicSrv!=null &&  musicBound  &&  musicSrv.isPng())
+            return musicSrv.getDur();
+        else return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if(musicSrv!=null && musicBound && musicSrv.isPng())
+            return musicSrv.getPosn();
+        else return 0;
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(musicSrv!=null && musicBound)
+            return musicSrv.isPng();
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    // ------------------------------- Override MessageListener method --------------------------------------
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        if(messageEvent.getPath().equals("/wear_message")) {
+            String msg = new String(messageEvent.getData());
+            updateCommand(msg);
+        }
+    }
+
+    // ------------------------------ Override ConnectionCallback methods ----------------------------------
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "connected");
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "onConnectionSuspended: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "connection failed");
+    }
+
+    // ------------------------------ onClick calls----------------------------------------------------------
+
+    public void songPicked(View view){
+
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+        if (!isPlaying()){
+            ((ImageButton)findViewById(R.id.Play)).setImageResource(R.drawable.img_btn_pause);
+        }
+    }
+
+    public void clickNext(View view) {
+
+        musicSrv.playNext();
+        final ImageButton button = (ImageButton) view.findViewById(R.id.Next);
+        button.setImageResource(R.drawable.img_btn_next_pressed);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                button.setImageResource(R.drawable.img_btn_next);
+            }
+        }, 500);
+    }
+
+    public void clickPrevious(View view) {
+
+        musicSrv.playPrev();
+        final ImageButton button = (ImageButton) view.findViewById(R.id.Previous);
+        button.setImageResource(R.drawable.img_btn_previous_pressed);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                button.setImageResource(R.drawable.img_btn_previous);
+            }
+        }, 500);
+    }
+
+    public void clickPlay(View view){
+
+        ImageButton button = (ImageButton) view.findViewById(R.id.Play);
+
+        if (isPlaying()){
+            musicSrv.pausePlayer();
+            button.setImageResource(R.drawable.img_btn_play);
+        } else {
+            musicSrv.playSong();
+            button.setImageResource(R.drawable.img_btn_pause);
+        }
+
+    }
+
+    // ------------------------- Methods ------------------------------------------------------------------
 
     public void getSongList() {
         //retrieve song info
@@ -192,281 +410,6 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         }
 
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-    public void songPicked(View view){
-
-        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
-        musicSrv.playSong();
-        if (!isPlaying()){
-            ((ImageButton)findViewById(R.id.Play)).setImageResource(R.drawable.img_btn_pause);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_shuffle:
-                musicSrv.setShuffle();
-                if (musicSrv.isShuffle()){
-                    item.setIcon(ContextCompat.getDrawable(this,R.drawable.img_btn_shuffle_pressed));
-                } else {
-                    item.setIcon(ContextCompat.getDrawable(this,R.drawable.img_btn_shuffle));
-                }
-                break;
-            case R.id.action_end:
-                stopService(playIntent);
-                musicSrv=null;
-                System.exit(0);
-                break;
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-        stopService(playIntent);
-        musicSrv=null;
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        //paused=true;
-        //Wearable.MessageApi.removeListener(mGoogleApiClient, this);
-        //mGoogleApiClient.disconnect();
-
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        /*if(paused){
-            //setController();
-            paused=false;
-        }*/
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        //controller.hide();
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
-
-        super.onStop();
-    }
-
-    @Override
-    public void pause() {
-        //playbackPaused=true;
-        musicSrv.pausePlayer();
-    }
-
-
-    @Override
-    public void seekTo(int pos) {
-        musicSrv.seek(pos);
-    }
-
-    @Override
-    public void start() {
-        musicSrv.go();
-    }
-    @Override
-    public int getDuration() {
-        if(musicSrv!=null &&  musicBound  &&  musicSrv.isPng())
-        return musicSrv.getDur();
-  else return 0;
-    }
-
-    @Override
-    public int getCurrentPosition() {
-        if(musicSrv!=null && musicBound && musicSrv.isPng())
-        return musicSrv.getPosn();
-        else return 0;
-    }
-
-    @Override
-    public boolean isPlaying() {
-        if(musicSrv!=null && musicBound)
-        return musicSrv.isPng();
-        return false;
-    }
-
-    @Override
-    public int getBufferPercentage() {
-        return 0;
-    }
-
-    @Override
-    public boolean canPause() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekBackward() {
-        return true;
-    }
-
-    @Override
-    public boolean canSeekForward() {
-        return true;
-    }
-
-    @Override
-    public int getAudioSessionId() {
-        return 0;
-    }
-
-    /*private void setController(){
-        controller = new MusicController(this);
-        controller.setPrevNextListeners(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playNext();
-            }
-        }, new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playPrev();
-            }
-        });
-        controller.setMediaPlayer(this);
-        controller.setAnchorView(findViewById(R.id.song_list));
-        controller.setEnabled(true);
-
-    }*/
-
-    public void clickNext(View view) {
-        musicSrv.playNext();
-        final ImageButton button = (ImageButton) view.findViewById(R.id.Next);
-        button.setImageResource(R.drawable.img_btn_next_pressed);
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //Do something after 100ms
-                button.setImageResource(R.drawable.img_btn_next);
-            }
-        }, 500);
-    }
-
-    public void clickPrevious(View view) {
-        musicSrv.playPrev();
-        final ImageButton button = (ImageButton) view.findViewById(R.id.Previous);
-        button.setImageResource(R.drawable.img_btn_previous_pressed);
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //Do something after 100ms
-                button.setImageResource(R.drawable.img_btn_previous);
-            }
-        }, 500);
-    }
-
-    public void clickPlay(View view){
-
-        ImageButton button = (ImageButton) view.findViewById(R.id.Play);
-
-        if (isPlaying()){
-            musicSrv.pausePlayer();
-            button.setImageResource(R.drawable.img_btn_play);
-        } else {
-            musicSrv.playSong();
-            button.setImageResource(R.drawable.img_btn_pause);
-        }
-
-
-    }
-
-    /*private void playNext(){
-        musicSrv.playNext();
-        if(playbackPaused){
-            setController();
-            playbackPaused=false;
-        }
-        controller.show(0);*/
-    //}
-
-    /*private void playPrev(){
-        musicSrv.playPrev();
-        if(playbackPaused){
-            setController();
-            playbackPaused=false;
-        }
-        controller.show(0);*/
-    //}
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.i(TAG, "connected");
-        Wearable.MessageApi.addListener(mGoogleApiClient, this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        Log.d(TAG, "onConnectionSuspended: " + cause);
-    }
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.e(TAG, "connection failed");
-    }
-
-
-
-    //@Override
-    /*public void onDataChanged(DataEventBuffer dataEvents) {
-
-        for(DataEvent event : dataEvents) {
-            if(event.getType() == DataEvent.TYPE_CHANGED) {
-                // DataItem changed
-                DataItem item = event.getDataItem();
-                if(item.getUri().getPath().compareTo("/Command") == 0) {
-                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
-                    updateCommand((String)dataMap.get(COMMAND_KEY));
-
-                }
-            } else if (event.getType() == DataEvent.TYPE_DELETED) {
-                // DataItem deleted
-            }
-        }
-    }*/
 
     public void updateCommand(final String mess) {
         runOnUiThread(new Runnable() {
@@ -494,11 +437,5 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         });
     }
 
-    @Override
-    public void onMessageReceived(MessageEvent messageEvent) {
-        if(messageEvent.getPath().equals("/wear_message")) {
-            String msg = new String(messageEvent.getData());
-            updateCommand(msg);
-        }
-    }
+
 }
